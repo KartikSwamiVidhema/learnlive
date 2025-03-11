82d3d5f5afc860e1fa677eed13c342b2fce39904
viddeveloperphp01 <gaurav.vaish@vidhema.com>
2025-03-11 12:19:03 +0530

    images folder

---
+++ public/wp-content/uploads/thwec_templates/admin_new_order.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
+<body><table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="thwecmf_template_wrapper"><tr><td align="center" class="thwecmf-template-wrapper-column" valign="top" style="background-color: #f7f7f7; padding: 70px 0;" bgcolor="#f7f7f7"><table id="tpf_t_builder" width="600" cellspacing="0" cellpadding="0" class="thwecmf-main-builder thwecmf-template-block" style="max-width: 600px; width: 600px; margin: auto;" align="center"><tr><td class="thwecmf-builder-column" style="vertical-align: top; border-radius: 2px; background-size: cover; background-repeat: no-repeat; background-position: top; border-style: solid; border-color: #dedede; background-color: #fff; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px;" bgcolor="#fff">
+<table class="thwecmf-row thwecmf-block-two-column thwecmf-builder-block" id="tpf_1011" cellpadding="0" cellspacing="0px" style="border-spacing: 0px; width: 100%; table-layout: fixed; max-width: 100%; margin: 0 auto; margin-top: 0px; margin-right: auto; margin-bottom: 0px; margin-left: auto; background-size: 100%; background-repeat: no-repeat; background-position: top; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; border-style: dotted; border-color: #ddd; padding-top: 12px; padding-right: 10px; padding-bottom: 12px; padding-left: 10px; background-color: #1f4b3f;" width="100%" align="center" bgcolor="#1f4b3f"><tr>
+<td class="thwecmf-column-padding thwecmf-col thwecmf-columns" id="tpf_1012" style="vertical-align: top; box-sizing: border-box; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; border-style: dotted; border-color: #ddd; word-break: break-word; padding: 10px 10px; text-align: center; background-size: 100%; background-position: top;" align="center"><table class="thwecmf-block thwecmf-block-image thwecmf-builder-block" id="tpf_1014" cellspacing="0" cellpadding="0" align="center" style="table-layout: fixed; width: 100%; height: auto; max-width: 600px; box-sizing: border-box;" width="100%">
+		<tr>
+			<td class="thwecmf-block-child thwecmf-image-column" style="padding: 0; box-sizing: border-box; text-align: center; vertical-align: middle;" align="center"><p style="padding: 10px 0px; margin: 0; width: 50%; display: inline-block; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px; border-style: none; border-color: transparent;" width="50%">
+			<img src="https://ithemes.xyz/wp-content/uploads/2023/07/itemes_logo2.png" alt="Image" width="133" height="37.1719" style="max-width: 100%; max-height: 100%; display: block; width: 133px; height: 37.1719px;">
+		</p></td>
+</tr>
+</table></td>
+<td class="thwecmf-column-padding thwecmf-col thwecmf-columns" id="tpf_1013" style="vertical-align: top; box-sizing: border-box; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; border-style: dotted; border-color: #ddd; word-break: break-word; padding: 10px 10px; text-align: center; background-size: 100%; background-position: top;" align="center"><table class="thwecmf-block thwecmf-block-text thwecmf-builder-block" id="tpf_1017" cellspacing="0" cellpadding="0" style='table-layout: fixed; width: 100%; color: #636363; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 13px; line-height: 22px; text-align: center; margin: 0 auto; box-sizing: border-box;' width="100%" align="center">
+		<tr>
+			<td class="thwecmf-block-child thwecmf-block-text-holder" style='vertical-align: top; box-sizing: border-box; color: #636363; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 13px; line-height: 150%; text-align: center; padding: 15px 15px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px; border-color: transparent; border-style: none; background-size: cover; background-repeat: no-repeat; background-color: transparent;' align="center" bgcolor="transparent">
+			<div class="wecmf-txt-wrap">New Order: #<?php if(isset($order)) : ?><?php echo $order->get_id();?><?php endif; ?><br>
+</div>
+			</td>
+		</tr>
+	</table></td>
+</tr></table>
+<table class="thwecmf-row thwecmf-block-one-column thwecmf-builder-block" id="tpf_1004" cellpadding="0" cellspacing="0px" style="border-spacing: 0px; width: 100%; table-layout: fixed; max-width: 100%; margin: 0 auto; background-size: 100%; background-repeat: no-repeat; background-position: top; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; margin-top: 0px; margin-right: auto; margin-bottom: 0px; margin-left: auto; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px; border-style: none; border-color: transparent; background-image: none; background-color: transparent;" width="100%" align="center" bgcolor="transparent"><tr><td class="thwecmf-column-padding thwecmf-col thwecmf-columns" id="tpf_1005" style="vertical-align: top; box-sizing: border-box; word-break: break-word; padding: 10px 10px; background-size: 100%; background-position: top; width: 100%; padding-top: 48px; padding-right: 48px; padding-bottom: 48px; padding-left: 48px; text-align: left; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px; border-style: none; border-color: transparent; background-image: none; background-color: transparent;" width="100%" align="left" bgcolor="transparent">
+<table class="thwecmf-block thwecmf-block-text thwecmf-builder-block" id="tpf_1006" cellspacing="0" cellpadding="0" style='table-layout: fixed; width: 100%; margin: 0 auto; box-sizing: border-box; color: #636363; font-size: 14px; line-height: 150%; font-weight: normal; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; margin-top: 0px; margin-right: auto; margin-bottom: 0px; margin-left: auto; text-align: left;' width="100%" align="center">
+		<tr style='color: #636363; font-size: 14px; font-weight: normal; line-height: 150%; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;'>
+			<td class="thwecmf-block-child thwecmf-block-text-holder" style='vertical-align: top; box-sizing: border-box; padding: 15px 15px; background-size: cover; background-repeat: no-repeat; color: #636363; font-size: 14px; font-weight: normal; line-height: 150%; text-align: left; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; background-color: transparent; background-image: none; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px; border-color: transparent; border-style: none; padding-top: 0px; padding-right: 0px; padding-bottom: 15px; padding-left: 0px;' align="left" bgcolor="transparent">
+			<div class="wecmf-txt-wrap" style='color: #636363; font-size: 14px; font-weight: normal; line-height: 150%; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;'>You’ve received the following order from <?php if(isset($order)) : ?><?php echo esc_html( $order->get_billing_first_name().' '.$order->get_billing_last_name() ); ?><?php elseif(isset($user_login)): ?><?php echo esc_html($user_login); ?><?php endif; ?>:<br style='color: #636363; font-size: 14px; font-weight: normal; line-height: 150%; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;'>
+</div>
+			</td>
+		</tr>
+	</table>
+<div class="thwecmf-hook-order-details"><?php if(isset($order)){ 
+ 					do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email ); 
+ 				}?></div>
+<?php if(isset($order)){ 
+  					do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email ); 
+  				}?>
+<?php if(isset($order)){ 
+  					do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email ); 
+  				}?>
+<table class="thwecmf-block thwecmf-block-text thwecmf-builder-block" id="tpf_1010" cellspacing="0" cellpadding="0" style='table-layout: fixed; width: 100%; margin: 0 auto; box-sizing: border-box; color: #636363; font-size: 14px; line-height: 150%; font-weight: normal; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; margin-top: 0px; margin-right: auto; margin-bottom: 0px; margin-left: auto; text-align: left;' width="100%" align="center">
+		<tr style='color: #636363; font-size: 14px; font-weight: normal; line-height: 150%; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;'>
+			<td class="thwecmf-block-child thwecmf-block-text-holder" style='vertical-align: top; box-sizing: border-box; padding: 15px 15px; background-size: cover; background-repeat: no-repeat; color: #636363; font-size: 14px; font-weight: normal; line-height: 150%; text-align: left; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; background-color: transparent; background-image: none; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px; border-color: transparent; border-style: none; padding-top: 10px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px;' align="left" bgcolor="transparent">
+			<div class="wecmf-txt-wrap" style='color: #636363; font-size: 14px; font-weight: normal; line-height: 150%; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;'>Congratulations on the sale.<br style='color: #636363; font-size: 14px; font-weight: normal; line-height: 150%; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;'>
+</div>
+			</td>
+		</tr>
+	</table>
+</td></tr></table>
+</td></tr></table></td></tr></table></body>
+</html>
